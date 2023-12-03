2f1627490c2b2d512362ad6a910caba676c2c4b5
Ermias <ermiasnigat17@gmail.com>
2023-12-03 20:52:34 +0300

    after .gitignore ignores node modules

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+   
+       <div >
+        <p className='hover:text-violet-600'>Home</p>
+       </div>
+   
+  )
+}
+
+export default Home
