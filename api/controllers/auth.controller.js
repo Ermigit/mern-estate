ce8ba7eabe539bd8a41f69477861efb33158c811
Ermias <ermiasnigat17@gmail.com>
2023-12-07 06:57:19 +0300

    wrote auth.controller logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,10 @@
+import User from '../models/user.model.js'
 const signup= (req,res)=>{
+   const {username, email ,password} = req.body;
+   const newUser = new User ({username, email, password});
+   newUser.save();
+
    console.log(req.body);
+
 }
 export default signup;
