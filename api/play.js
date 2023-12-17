16821f487a3cb2f7aaeb5e8a50555bfdda1babbb
Ermias <ermiasnigat17@gmail.com>
2023-12-17 20:33:23 +0300

    placebo

---
+++ api/play.js
@@ -0,0 +1,8 @@
+// a program that accept two numbers from user and display the addition of that two numbers
+// input : 10 20
+// output : 30  
+const addition= (x,y)=>{
+return x+y
+}
+
+console.log(addition(2,5))
