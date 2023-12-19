77e2de5ae61b8db8b3e7abe3ea4677d84bddf864
Ermias <ermiasnigat17@gmail.com>
2023-12-19 12:28:26 +0300

    added redux-persist

--- api/play.js
+++ api/play.js
@@ -1,8 +1,9 @@
-// a program that accept two numbers from user and display the addition of that two numbers
-// input : 10 20
-// output : 30  
-const addition= (x,y)=>{
-return x+y
+
+import React from 'react'
+function play() {
+  return (
+    <div>play</div>
+  )
 }
 
-console.log(addition(2,5))
+export default play
