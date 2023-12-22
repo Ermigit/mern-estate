06c453b75668456c82adb024c81321e0255e9710
Ermias <ermiasnigat17@gmail.com>
2023-12-22 16:23:57 +0300

    added image selector for in profile page and reference to the avator

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
 
 function Profile() {
+  const fileRef = new useRef();
   const {currentUser} = useSelector(state => state.user)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
     <form className='flex flex-col gap-4'>
-      <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 ' src={currentUser.avatar} alt='Profile'></img>
+      <input type='file' ref={fileRef}/>
+      <img onClick={()=>fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 ' src={currentUser.avatar} alt='Profile'></img>
        <input type='text' placeholder='username'id="username" className='border p-3 rounded-lg'/>
        <input type='email' placeholder='email'id="email" className='border p-3 rounded-lg'/>
        <input type='password ' placeholder='password'id="password" className='border p-3 rounded-lg'/>
