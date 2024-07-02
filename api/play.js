50c448f49fa573b2839bfeaeb33f1f4051bf1040
Ermias <ermiasnigat17@gmail.com>
2024-07-02 12:22:36 +0300

    Play verification

--- api/play.js
+++ api/play.js
@@ -1,13 +1,1 @@
-const P = new Promise((resolve,reject)=>{
-    const crealelistingMAX = 6
-    if (crealelistingMAX>6)
-        {resolve("you can create 6 listing")
-        }
-     else(reject("you can not create 6 listing"))   
-})
-P.then((msg)=>{
-    console.log(" message"+msg)
-})
-.catch((msg)=>{
-    console.log("message "+msg)
-})
+// updated on verifcation
